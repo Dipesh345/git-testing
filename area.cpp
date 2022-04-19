7b9ab846160fd958b8cbd7eb506d53c816c7aaf7
Dipesh345 <thapadipesh722@gmail.com>
2022-04-19 12:30:59 +0545

    pubg

---
+++ area.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int a,b;
+	printf("enter two numbers");
+	scanf("%d%d",&a,&b);
+	if(a>b){
+	printf("%d is maximum",&a);
+	}
+	else{
+		printf("%d is maximum",&b);
+		}
+	
+	getch();
+return 0;
+
+}
